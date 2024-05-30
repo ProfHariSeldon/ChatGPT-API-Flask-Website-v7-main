cc83d25ce0916f58cd5128684a14c17b1be3c46a
ProfHariSeldon <linuxborg2@yahoo.com>
2024-05-30 15:52:24 -0400

    Faster chromosome graphs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # pip install Flask openai
 # openai migrate
 
-with open("./static/chromosomes.py") as f:
-    exec(f.read())
+##with open("./static/chromosomes.py") as f:
+##    exec(f.read())
 
 from flask import Flask, request, render_template, redirect
 import openai
@@ -29,7 +29,7 @@ app = Flask(__name__)
 client = openai.OpenAI(
     # This is the default and can be omitted
     api_key = os.environ.get("OPENAI_API_KEY"),
-    # assistant_id = os.environ.get('ASSISTANT_ID')
+    # assistant_id = os.environ.get('ASSISTANT_ID')/
 )
 
 # https://stackoverflow.com/questions/78018805/how-to-execute-custom-actions-with-chatgpt-assistants-api
@@ -65,27 +65,27 @@ from openai import AssistantEventHandler
 # First, we create a EventHandler class to define
 # how we want to handle the events in the response stream.
  
-class EventHandler(AssistantEventHandler):    
-  @override
-  def on_text_created(self, text) -> None:
-    print(f"\nassistant > ", end="", flush=True)
+class EventHandler(AssistantEventHandler):
+    @override
+    def on_text_created(self, text) -> None:
+        print(f"\nassistant > ", end="", flush=True)
       
-  @override
-  def on_text_delta(self, delta, snapshot):
-    print(delta.value, end="", flush=True)
+    @override
+    def on_text_delta(self, delta, snapshot):
+        print(delta.value, end="", flush=True)
       
-  def on_tool_call_created(self, tool_call):
-    print(f"\nassistant > {tool_call.type}\n", flush=True)
+    def on_tool_call_created(self, tool_call):
+        print(f"\nassistant > {tool_call.type}\n", flush=True)
   
-  def on_tool_call_delta(self, delta, snapshot):
-    if delta.type == 'code_interpreter':
-      if delta.code_interpreter.input:
-        print(delta.code_interpreter.input, end="", flush=True)
-      if delta.code_interpreter.outputs:
-        print(f"\n\noutput >", flush=True)
-        for output in delta.code_interpreter.outputs:
-          if output.type == "logs":
-            print(f"\n{output.logs}", flush=True)
+    def on_tool_call_delta(self, delta, snapshot):
+        if delta.type == 'code_interpreter':
+            if delta.code_interpreter.input:
+                print(delta.code_interpreter.input, end="", flush=True)
+            if delta.code_interpreter.outputs:
+                print(f"\n\noutput >", flush=True)
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        print(f"\n{output.logs}", flush=True)
  
 # Then, we use the `create_and_stream` SDK helper 
 # with the `EventHandler` class to create the Run 
@@ -106,17 +106,20 @@ def index():
     prompt = ""
     response = ""
     graph = ""
+    selected_graph = ""
+    
     if request.method == 'POST':
-        prompt = request.form['prompt']
+        prompt = request.form.get('prompt')
         selected_graph = request.form.get('graph')
         
         if prompt:
             try:
                 # completion = openai.Completion.create(
                 completion = client.chat.completions.create(
-                model='gpt-3.5-turbo',
-                # assistant_id=assistantId,
-                messages=[{"role": "user", "content": prompt}]
+                    ## model='gpt-3.5-turbo',
+                    model='gpt-4-turbo',
+                    # assistant_id=assistantId,
+                    messages=[{"role": "user", "content": prompt}]
                     # engine="text-davinci-003",
                     # engine="gpt-3.5-turbo",  # You can use other models like "gpt-3.5-turbo"
                     # prompt=prompt,
@@ -126,65 +129,72 @@ def index():
                     # frequency_penalty=0.0,
                     # presence_penalty=0.0
                 )
-                # response = completion.choices[0].text.strip()
                 response = completion.choices[0].message.content
-                ## response = message.choices[0].message.content
-                
+            except Exception as e:
+                response = f"An error occurred: {str(e)}"
+
+        if selected_graph:
+            try:
                 print("Selected Graph = ", selected_graph)
-                # Determine the graph to display based on the selected radio button
-                if selected_graph == 'chr1':
-                    graph = '../static/images/chr1.png'
-                elif selected_graph == 'chr2':
-                    graph = '../static/images/chr2.png'
-                elif selected_graph == 'chr3':
-                    graph = '../static/images/chr3.png'
-                elif selected_graph == 'chr4':
-                    graph = '../static/images/chr4.png'
-                elif selected_graph == 'chr5':
-                    graph = '../static/images/chr5.png'
-                elif selected_graph == 'chr6':
-                    graph = '../static/images/chr6.png'
-                elif selected_graph == 'chr7':
-                    graph = '../static/images/chr7.png'
-                elif selected_graph == 'chr8':
-                    graph = '../static/images/chr8.png'
-                elif selected_graph == 'chr9':
-                    graph = '../static/images/chr9.png'
-                elif selected_graph == 'chr10':
-                    graph = '../static/images/chr10.png'
-                elif selected_graph == 'chr11':
-                    graph = '../static/images/chr11.png'
-                elif selected_graph == 'chr12':
-                    graph = '../static/images/chr12.png'
-                elif selected_graph == 'chr13':
-                    graph = '../static/images/chr13.png'
-                elif selected_graph == 'chr14':
-                    graph = '../static/images/chr14.png'
-                elif selected_graph == 'chr15':
-                    graph = '../static/images/chr15.png'
-                elif selected_graph == 'chr16':
-                    graph = '../static/images/chr16.png'
-                elif selected_graph == 'chr17':
-                    graph = '../static/images/chr17.png'
-                elif selected_graph == 'chr18':
-                    graph = '../static/images/chr18.png'
-                elif selected_graph == 'chr19':
-                    graph = '../static/images/chr19.png'
-                elif selected_graph == 'chr20':
-                    graph = '../static/images/chr20.png'
-                elif selected_graph == 'chr21':
-                    graph = '../static/images/chr21.png'
-                elif selected_graph == 'chr22':
-                    graph = '../static/images/chr22.png'
-                elif selected_graph == 'chrMT':
-                    graph = '../static/images/chrMT.png'
-                elif selected_graph == 'chrX':
-                    graph = '../static/images/chrX.png'
-                elif selected_graph == 'chrY':
-                    graph = '../static/images/chrY.png'
+                    
+                import pandas as pd
+                import numpy as np
+                import matplotlib.pyplot as plt
+
+                files_paths = {
+                    'genes': './static/hg38_genes.bed',
+                    'introns': './static/hg38_introns.bed',
+                    'CDS': './static/hg38_CDS.bed',
+                    '5p': './static/hg38_5p.bed',
+                    '3p': './static/hg38_3p.bed',
+                    'circRNA': './static/hsa_hg38_circRNA.bed',
+                    'exons' : './static/hg38_exons.bed'
+                }
+                bed_dfs = {}
+
+                for key, path in files_paths.items():
+                    bed_dfs[key] = pd.read_csv(path, sep='\t', header=None)
+
+                def adjust_frequency_for_chromosome(df, chromosome, bins):
+                    df_chr = df[df[0] == chromosome]
+                    positions = df_chr[1]._append(df_chr[2])
+                    freq, bin_edges = np.histogram(positions, bins=bins)
+                    return freq, bin_edges
+                    
+                chromosome = selected_graph
+                bins = 100
+                frequencies = {}
+                bin_edges_dict = {}
+                for key, df in bed_dfs.items():
+                    frequencies[key], bin_edges_dict[key] = adjust_frequency_for_chromosome(df, chromosome, bins)
+
+                plt.figure(figsize=(15, 10))
+                for key, freq in frequencies.items():
+                    plt.plot(bin_edges_dict[key][:-1], freq, label=f'{key} Frequency')
+                
+                chromosome_number = chromosome[3:]
+                if chromosome_number == 'MT':
+                    chromosome_number = 'ChrMitochondria'
+                    plt.xlabel(f'Genomic Position on Mitochondiral Chromosome')
+                    plt.ylabel('Frequency')
+                    plt.title(f'Frequency of Genomic Features on Mitochondiral Chromosome')
+                    plt.legend()
+                    plt.savefig(f'./static/images/{chromosome_number}.png')
+                    plt.close()
+                    graph = f'../static/images/{chromosome_number}.png'
+                else:
+                    plt.xlabel(f'Genomic Position on Chromosome {chromosome_number}')
+                    plt.ylabel('Frequency')
+                    plt.title(f'Frequency of Genomic Features on Chromosome {chromosome_number}')
+                    plt.legend()
+                    plt.savefig(f'./static/images/{chromosome_number}.png')
+                    plt.close()
+                    graph = f'../static/images/{chromosome_number}.png'
+                    
                 print("graph path = ", graph)
             except Exception as e:
                 response = f"An error occurred: {str(e)}"
+                
     return render_template('index.html', prompt=prompt, response=response, graph=graph)
 
 if __name__ == '__main__':
